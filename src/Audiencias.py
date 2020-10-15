41e0ce93e017619bb134df10ed82f1152945e5bd
yisuscc <jescarcar5@alum.us.es>
2020-10-15 11:03:28 +0200

    hola

---
+++ src/Audiencias.py
@@ -0,0 +1,15 @@
+'''
+Created on 15 oct. 2020
+
+@author: jesus
+'''
+import csv
+import statistics
+from matplotlib import pyplot as plt
+
+
+# 1ee_audiencias
+'''
+el fichero contiene una columna correspondiente al nuumero de temproada 
+'''
+def lee_audiencias(fichero):
