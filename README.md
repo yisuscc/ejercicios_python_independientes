3fe009a4815846d3696d64a063a939dc85f6137e
yisuscc <jescarcar5@alum.us.es>
2020-10-15 11:34:36 +0200

    lo subo a mig solo y exclusivamente para tener una copia de seguridad anets de experimentar con matplolib(que no me va)

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ejercicios_python_independientes
+En este no tengo tanta solvencia en el primer ejercicio, puesto que acostumbro a utilizar tuplas con nombre
--- src/Audiencias.py
+++ src/Audiencias.py
@@ -10,6 +10,26 @@ from matplotlib import pyplot as plt
 
 # 1ee_audiencias
 '''
-el fichero contiene una columna correspondiente al nuumero de temproada 
+el fichero contiene una columna correspondiente al nuumero de temporada 
+y al porcentaje de la audiencia. 
 '''
 def lee_audiencias(fichero):
+    '''
+    Nos piden que hagamos una lista(corchetes y mutable) con los porcentajes del share, tenemos que separar los datos, que están divididos con comas
+    '''
+    audiencias = [] 
+    with open (fichero,'r',encoding='utf-8') as f: 
+        # hay que ir línea por línea. las lineas son f
+        for e in f:
+            #separarmos los elementos entre comas
+            temporada, share = e.split(',')
+            
+            temporada = int(temporada)
+            share = float(share)
+            
+            #yo habia entendido una lista con solo el share , y no una lista de tuplas.
+             
+            temp_y_share = (temporada, share) 
+            audiencias.append(temp_y_share)
+    return audiencias
+            
